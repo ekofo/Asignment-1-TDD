6eb3e1bc671b8cb08bde2eb74d4e43ec91d1fb41
siya981 <smqhamane98@gmail.com>
2021-04-01 20:58:11 +0200

    Add two numbers

---
+++ src/main/java/za/ac/cput/calculate1/AddTwoNumners.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.addition1;
+
+/**
+ *
+ * @author Siyathandaza
+ */
+public class AddTwoNumners {
+    public static void main(String[] args) { 
+        int num1=6,num2=15;
+        int sum= num1+num2;
+        System.out.println("Sum of these numbers: "+ sum);
+}
+  
+}
